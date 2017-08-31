deaa769f47282c0035e331e512def5174f1fc5d9
zhirong.li <zhirong.li@alo7.com>
2017-08-31 14:24:05 +0800

    timer locker init

---
+++ src/Component.vue
@@ -0,0 +1,87 @@
+<template>
+  <div id="timer-mask" v-show="visible">
+    <div class="timer-clock">
+      <p class="title">你的操作过于频繁，请稍后重试！</p>
+      <div class="time-num">{{leftime}}s</div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ["timeCount", "showmask"],
+  data() {
+    return {
+      initTime: 60,
+      leftime: "60",
+      flag: false,
+      count: 0,
+      visible: this.showmask
+    }
+  },
+  methods: {
+    timeDown(_this) {
+      _this.count++;
+      console.log("count:" + _this.count);
+      _this.initTime--;
+      let leftime = _this.initTime;
+      if (leftime <= 0) {
+        _this.flag = true;
+        _this.visible = false;
+      }
+      _this.leftime = leftime;
+    },
+    format(time) {
+      console.log("format-time:" + time);
+      if (time >= 10) {
+        return time
+      } else {
+        return `0${time}`
+      }
+    }
+  },
+  mounted() {
+    let _this = this;
+    _this.initTime = _this.timeCount;
+    let time = setInterval(function() {
+      if (_this.flag == true) {
+        clearInterval(time);
+      } else {
+        _this.timeDown(_this);
+      }
+    }, 1000);
+  }
+}
+</script>
+<style lang="scss" scoped>
+body {
+  margin: 0;
+  padding: 0;
+}
+
+#timer-mask {
+  background-color: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  .timer-clock {
+    font-family: "PingFangSC-Regular";
+    background-color: #fff;
+    width: 426px;
+    height: 208px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    margin-left: -213px;
+    margin-top: -104px;
+    text-align: center;
+    color: #4A4A4A;
+  }
+  .title {
+    font-size: 14px;
+    margin-top: 36px;
+  }
+  .time-num {
+    font-size: 48px;
+  }
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './Component.vue'
+
+//module.exports = App;
+
+/*  下面为单个组件需要测试时挂载请解开以下注释  */
+new Vue({
+  el:'#app',
+  data(){
+    return {
+      timeCount:60,
+      showmask: true
+    }
+  },
+  template:"<App :timeCount='timeCount' :showmask='showmask'></App>",
+  components:{
+    "App":App
+  }
+})
